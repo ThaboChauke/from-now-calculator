1bf66b8af6a54bfc4a2de3715f01421bf4441b8c
ThaboChauke <chaukethabo16@gmail.com>
2025-03-04 14:48:55 +0200

    show results, show toats

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let submitBtn = document.getElementById("submitBtn")
+let resetBtn = document.getElementById("resetBtn")
+
 const currentMinsEl = document.getElementById("currentMinutes")
 const currentHoursEl = document.getElementById("currentHours")
 const currentSecondsEl = document.getElementById("currentSeconds")
+
+const userMinsEl = document.getElementById("userMinutes")
+const userHoursEl = document.getElementById("userHours")
+const userSecondsEl = document.getElementById("userSeconds")
+const newDateEl = document.getElementById("date")
 
 const runClock = setInterval(function currentTime() {
     let dateObject = new Date()
@@ -17,10 +24,46 @@ const runClock = setInterval(function currentTime() {
     currentMinsEl.textContent = minutes
     currentSecondsEl.textContent = seconds
 
-    console.log(hours, minutes, seconds)
+    console.log(dateObject.getDay())
+
 }, 1000)
 
 
 submitBtn.addEventListener("click", function() {
-    console.log("here")
+    let dateObject = new Date()
+
+    if (userHoursEl.value !== null) dateObject.setHours(dateObject.getHours() + +userHoursEl.value)
+    if (userMinsEl.value !== null) dateObject.setMinutes(dateObject.getMinutes() + +userMinsEl.value)
+    if (userSecondsEl.value !== null) dateObject.setSeconds(dateObject.getSeconds() + +userSecondsEl.value)
+    
+    if (userSecondsEl.value === "" && userMinsEl.value === "" && userHoursEl.value === "") {
+        showToast()
+    } else {
+        userHoursEl.value = dateObject.getHours()
+        userMinsEl.value = dateObject.getMinutes()
+        userSecondsEl.value = dateObject.getSeconds()
+
+        userHoursEl.disabled = true
+        userMinsEl.disabled = true
+        userSecondsEl.disabled = true
+
+        const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+        const dayName = days[dateObject.getDay()]
+
+        newDateEl.textContent = dayName + " " + dateObject.getDate() + "/" + dateObject.getMonth() + "/" + dateObject.getFullYear()
+        newDateEl.classList.add("show")
+        resetBtn.classList.add("show")
+    }
 })
+
+resetBtn.addEventListener("click", function() {
+    window.location.reload()
+})
+
+function showToast() {
+    const toast = document.getElementById("toast")
+    toast.classList.add("show")
+    setTimeout(() => {
+        toast.classList.remove("show")
+    }, 3000)
+}
